3695d4162751908337901fe0a01669f0d6ded7c0
Vaughn Newman <rwaters@ultrafark.com>
2015-07-30 20:07:03 -0400

    blissify
    Change-Id: I0f9f0a30c907d887f346554958e7acee3b3e3b91

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,3 +158,21 @@ MALLOC_IMPL := dlmalloc
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 BOARD_HARDWARE_CLASS := device/lge/mako/cmhw/
+
+# LZMA Compression
+WITH_LZMA_OTA := true
+
+# BlissPop Config Flags
+TARGET_TC_ROM := 4.8
+TARGET_TC_KERNEL := 4.9-linaro
+BLISSIFY := true
+BLISS_O3 := true
+BLISS_STRICT := true
+BLISS_GRAPHITE := true
+BLISS_KRAIT := true
+BLISS_PIPE := true
+TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+
+#SaberMod
+-include vendor/bliss/config/sm.mk
